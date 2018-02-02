c98998a9e567e333775eeca3051f458f60549ba4
Henrik Dobbe Flemmen <henrikdf@stud.ntnu.no>
2018-02-02 09:04:18 +0100

    Resten av det autogenererte prosjektet til clion

---
+++ cmake-build-debug/CMakeFiles/gitDemo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gitDemo.dir/main.c.o"
+  "gitDemo.pdb"
+  "gitDemo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/gitDemo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
